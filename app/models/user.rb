a2394867725d5c49529c251f3cee94663770ce61
Your Name <inahsanjuan@gmail.com>
2018-06-14 13:14:49 +0800

    Full project

---
+++ app/models/user.rb
@@ -0,0 +1,7 @@
+class User < ApplicationRecord
+    validates :name, presence: true,
+                     length: { minimum: 5 }
+
+    validates :email, presence: true,
+                    length: { minimum: 5 }
+end
